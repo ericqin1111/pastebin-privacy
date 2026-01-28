d1fa887f3b2ce019dea82b0f61d6da85eea8b195
ubuntueric <187919712@qq.com>
2026-01-28 09:37:14 +0800

    chore: init spring boot backend

---
+++ src/main/java/com/example/pastebin/PastebinBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.pastebin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastebinBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PastebinBackendApplication.class, args);
+    }
+}
